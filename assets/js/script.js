8ef8794113a469e00ebdbe0f21120d301406d344
CarolinaOrellana <caryorellanam@gmail.com>
2022-05-10 17:00:16 -0400

    -Sección formulario de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,15 @@ window.addEventListener('scroll', function(){
 })
 
 //tooltip (en cards e iconos de redes sociales) 
-/*Bootstrap 5.1*/
+//Bootstrap 5.1
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+//Popover botón de contacto 
+//Bootstrap 5.1
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
 })
